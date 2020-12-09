50f97d730b59e9ffe06ed58d18f0723dfb1a12e1
Francis Lavoie <lavofr@gmail.com>
2020-12-09 17:29:01 -0500

    Set up laravel-websockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,13 +16,18 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    encrypted: (window.location.protocol === 'https:'),
+    wsHost: window.location.hostname,
+    wsPort: window.location.port,
+    wssPort: window.location.port,
+    disableStats: true,
+    enabledTransports: ['ws', 'wss'],
+});
